6b61a5ffa52b1c3cf004d037f10cd9be8402db46
CecileKafrouni <64487789+CecileKafrouni@users.noreply.github.com>
2020-06-17 22:21:10 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,6 +8,9 @@
 import pandas as pd
 
 import Preparation_CSV as pc        
+import feature_importances_ as fi
+import Correlation as cor
+import Normalisation_donnees as nor
 
 df_T_15 = pd.read_csv("Thursday-15-02-2018_TrafficForML_CICFlowMeter.csv")
 df_T_22 = pd.read_csv("Thursday-22-02-2018_TrafficForML_CICFlowMeter.csv")
@@ -36,5 +39,23 @@ df_F_23_copy.to_csv("df_F_23_copy.csv", sep=';')
 # Concaténation de toutes les datasets
 dataset = pd.concat([df_T_15_copy,df_T_22_copy,df_W_21_copy,df_W_14_copy,df_F_02_copy,df_F_23_copy])
 dataset = dataset.reset_index(drop = True)
+dataset.fillna(0, inplace=True)
 dataset.to_csv("total_dataset_Equalized_intrusion_SinNull.csv", sep=';', index = False)
 
+# Importances des features
+df_features_importances = fi.FeaturesImportances(dataset)
+
+# On enregistre dans un csv ce dataframe pour le sauvegarder
+df_features_importances = df_features_importances.reset_index(drop = True)
+df_features_importances.to_csv("total_dataset_Features_importances.csv", sep=';', index = False)
+
+# Correlation
+df_correle = cor.Correlation(df_features_importances)
+df_correle = df_correle.reset_index(drop = True)
+df_correle.to_csv("total_dataset_correles.csv", sep=';', index = False)
+
+
+# Normalisation des donnees
+df_normalise = nor.NormalizeDf(df_correle)
+df_normalise = df_normalise.reset_index(drop = True)
+df_normalise.to_csv("total_dataset_normalise.csv", sep=';', index = False)
