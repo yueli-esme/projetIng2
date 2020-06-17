97092d65e43b87ba2634c78d9a3b581f851cf707
CecileKafrouni <64487789+CecileKafrouni@users.noreply.github.com>
2020-06-17 22:21:05 +0200

    Create Correlation.py

---
+++ Correlation.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+'''
+------------------------------ Correlation -----------------------------------
+'''
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def Correlation(df):
+ 
+    df['Intrusion'].value_counts()
+     
+    plt.figure(figsize=(20,20))
+    sns.heatmap(df.corr())
+    sns.heatmap(df.corr(),annot=True)
+    
+    df_corr = df.corr()
+    df_copy = df.copy()
+    
+    for i in range(0,len(df_corr)):
+        if (df_corr.iloc[i]['Intrusion'] < 0.2) and (df_corr.iloc[i]['Intrusion'] > -0.2):
+            print(df_corr.iloc[i])
+            del df_copy[df_corr.iloc[i].name]
+    
+    return df_copy
+
