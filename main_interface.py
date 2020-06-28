aa8c6bc2a25c81cf8746ab487d853f4a490c2b50
CecileKafrouni <64487789+CecileKafrouni@users.noreply.github.com>
2020-06-28 23:04:53 +0200

    Update main_interface.py

--- main_interface.py
+++ main_interface.py
@@ -4,19 +4,26 @@
 ----------------------- MAIN INTERFACE ---------------------------
 '''
 
-import Main
 
 import Modeles_ML as ml
-
 import GUI_Machine_Learning as gui
+import pickle
+import pandas as pd
 
+df = pd.read_csv("total_dataset_normalise.csv", sep=';')
 
-df_test=gui.interface()
+df_test=gui.interface(df)
 
-pred_knn = ml.knnPrediction(df_test, Main.knn)
-pred_rf = ml.rfPrediction(df_test, Main.rf)
-pred_gnb = ml.gnbPrediction(df_test, Main.gnb)
+filename_knn = 'finalized_model_knn.sav'
+filename_rf= 'finalized_model_rf.sav'
+filename_gnb = 'finalized_model_gnb.sav'
+
+loaded_model_knn = pickle.load(open(filename_knn, 'rb'))
+loaded_model_rf = pickle.load(open(filename_rf, 'rb'))
+loaded_model_gnb = pickle.load(open(filename_gnb, 'rb'))
+
+pred_knn = ml.knnPrediction(df_test, loaded_model_knn)
+pred_rf = ml.rfPrediction(df_test, loaded_model_rf)
+pred_gnb = ml.gnbPrediction(df_test, loaded_model_gnb)
 
 gui.result(pred_knn,pred_rf,pred_gnb)
-
-
